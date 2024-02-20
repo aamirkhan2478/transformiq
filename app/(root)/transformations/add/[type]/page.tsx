9a1e2c382faecf26c2288d6f08afadaee6562cef
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-02-20 22:19:24 +0500

    Add user authentication and redirect to sign-in page if user is not logged in

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,31 @@
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AddTransformationPage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationPage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
+
+  console.log(userId);
+
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          creditBalance={user.creditBalance}
+          type={transformation.type as TransformationTypeKey}
+        />
+      </section>
     </>
   );
 };
