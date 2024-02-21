8281a244750920d0434a2e8b93e104d4cd8e692c
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-02-21 11:35:41 +0500

    Add cloudinary integration and implement getAllImages function

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -1,16 +1,18 @@
+"use server";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
+import { v2 as cloudinary } from "cloudinary";
 
 // POPULATE USER
 const populateUser = (query: any) =>
   query.populate({
     path: "author",
     model: User,
-    select: "_id firstName lastName",
+    select: "_id firstName lastName clerkId",
   });
 
 // ADD IMAGE
@@ -31,6 +33,7 @@ export async function addImage({ image, path, userId }: AddImageParams) {
     handleError(error);
   }
 }
+
 // UPDATE IMAGE
 export async function updateImage({ image, path, userId }: UpdateImageParams) {
   try {
@@ -54,6 +57,7 @@ export async function updateImage({ image, path, userId }: UpdateImageParams) {
     handleError(error);
   }
 }
+
 // DELETE IMAGE
 export async function deleteImage(imageId: string) {
   try {
@@ -66,6 +70,7 @@ export async function deleteImage(imageId: string) {
     redirect("/");
   }
 }
+
 // GET IMAGE
 export async function getImageById(imageId: string) {
   try {
@@ -76,6 +81,63 @@ export async function getImageById(imageId: string) {
     if (!image) throw new Error("Image not found");
 
     return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET IMAGES
+export async function getAllImages({
+  limit = 9,
+  page = 1,
+  searchQuery = "",
+}: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder:transformiq";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
+    }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+    let query = {};
+    if (searchQuery) {
+      query = {
+        publicId: { $in: resourceIds },
+      };
+    }
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(
+      Image.find(query).sort({ updatedAt: -1 }).skip(skipAmount).limit(limit)
+    );
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages,
+    };
   } catch (error) {
     handleError(error);
   }
